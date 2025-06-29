97761ea3dd779328140d930f0cab04916329b8a4
TtPhong-FS <nam71441@gmail.com>
2025-06-29 09:17:22 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || '/Ecommerce-graybee-ban-may-tinh-linh-kien-dien-tu',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
