55d4527476fadc68032291f16a92047c21f0e311
TtPhong-FS <nam71441@gmail.com>
2025-06-29 09:29:49 +0700

    update

--- src/features/product/components/ProductCategories.jsx
+++ src/features/product/components/ProductCategories.jsx
@@ -11,15 +11,17 @@ function ProductCategories() {
   }))
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md mb-8">
-      <h1 className="mb-6">Danh mục sản phẩm</h1>
-      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-8 xl:grid-cols-10 gap-3 sm:gap-4">
-        {categories.map((category, index) => {
-          const imageCategory = renderImageProductCategory(category.name)
-          return <CategoryItem key={index} name={category.name} slug={category.slug} imageUrl={imageCategory} />
-        })}
+    categories?.length > 0 && (
+      <div className="bg-white p-6 rounded-lg shadow-md mb-8">
+        <h1 className="mb-6">Danh mục sản phẩm</h1>
+        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-8 xl:grid-cols-10 gap-3 sm:gap-4">
+          {categories.map((category, index) => {
+            const imageCategory = renderImageProductCategory(category.name)
+            return <CategoryItem key={index} name={category.name} slug={category.slug} imageUrl={imageCategory} />
+          })}
+        </div>
       </div>
-    </div>
+    )
   )
 }
 
